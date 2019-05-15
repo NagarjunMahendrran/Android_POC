8fe0137826718027f8dbe9404b07b7327802c216
Nagarjun M (Communications-Telecom Equipment) <NA316477@wipro.com>
2019-05-15 21:31:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/technicolor/technicolorthomson/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.technicolor.technicolorthomson;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    static EditText email_id_input;
+    static EditText password_input;
+    static String regex = "^(.+)@(.+)$";
+    static Pattern pattern ;
+    static Matcher match;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email_id_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.password_input);
+        pattern = Pattern.compile(regex);
+    }
+
+    public void onClick(View view) {
+        Button bt = (Button) view;
+        if(bt.getId() == R.id.sign_up){
+            Intent myIntent = new Intent(this, Registation.class);
+            startActivity(myIntent);
+        }
+        if(bt.getId() == R.id.sign_in){
+            if(validation()){
+                Log.e("I am okay","I am okay");
+            }else{
+                Log.e("I am not okay","I am not okay");
+            }
+
+        }
+
+    }
+
+    public boolean validation() {
+        match = pattern.matcher(email_id_input.getText());
+        if (TextUtils.isEmpty(email_id_input.getText())) {
+            email_id_input.setError("Email-ID/UserName should not be Empty");
+            email_id_input.setBackgroundResource(R.drawable.error_border);
+            return false;
+        }else if(!match.matches()){
+            email_id_input.setError("Please enter valid Email-ID");
+            email_id_input.setBackgroundResource(R.drawable.error_border);
+            return false;
+        }else {
+            email_id_input.setError(null);
+            email_id_input.clearFocus();
+            email_id_input.setBackgroundResource(R.drawable.success_border);
+        }
+        if (TextUtils.isEmpty(password_input.getText())) {
+            password_input.setError("Password should not be Empty");
+            password_input.setBackgroundResource(R.drawable.error_border);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/technicolor/technicolorthomson/Registation.java
@@ -0,0 +1,116 @@
+package com.example.technicolor.technicolorthomson;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Registation extends AppCompatActivity {
+    static EditText username;
+    static EditText email_id;
+    static EditText password;
+    static EditText serial_id;
+    static String regex = "^(.+)@(.+)$";
+    static Pattern pattern ;
+    static Matcher match;
+    static Snackbar snackbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registation);
+        username = findViewById(R.id.signup_input_name);
+        email_id = findViewById(R.id.signup_input_email);
+        password = findViewById(R.id.signup_input_password);
+        serial_id = findViewById(R.id.serial_id_text);
+        pattern = Pattern.compile(regex);
+    }
+
+    public void onClick(View view) {
+        Button bt = (Button) view;
+        if(bt.getId() == R.id.cancel){
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
+        }
+        if(bt.getId() == R.id.btn_signup){
+            if(validation()){
+                Log.e("I am okay","I am okay");
+            }else{
+                CoordinatorLayout Clayout = (CoordinatorLayout)findViewById(R.id.snackbarlocation);
+                snackbar = Snackbar.make(Clayout, "Please check the inputs!!!!", Snackbar.LENGTH_SHORT);
+                View snackBarView = snackbar.getView();
+                snackBarView.setBackgroundColor(Color.RED);
+                snackbar.show();
+
+            }
+
+        }
+    }
+
+    public boolean validation() {
+        Boolean check = true;
+         match = pattern.matcher(email_id.getText());
+        if (TextUtils.isEmpty(username.getText())) {
+            username.setError("UserName should not be Empty");
+            username.setBackgroundResource(R.drawable.error_border);
+            check =  false;
+        }
+        else if(TextUtils.isDigitsOnly(username.getText())){
+            username.setError("Only Digits not allowed");
+            username.setBackgroundResource(R.drawable.error_border);
+            check =  false;
+        }else {
+            username.setError(null);
+            username.clearFocus();
+            username.setBackgroundResource(R.drawable.success_border);
+        }
+
+        if (TextUtils.isEmpty(email_id.getText())) {
+            email_id.setError("Email-ID should not be Empty");
+            email_id.setBackgroundResource(R.drawable.error_border);
+            check =  false;
+        }
+        else if(!match.matches()){
+            email_id.setError("Please enter valid Email-ID");
+            email_id.setBackgroundResource(R.drawable.error_border);
+            check =  false;
+        }
+        else {
+            email_id.setError(null);
+            email_id.clearFocus();
+            email_id.setBackgroundResource(R.drawable.success_border);
+        }
+        if (TextUtils.isEmpty(password.getText())) {
+            password.setError("Password should not be Empty");
+            password.setBackgroundResource(R.drawable.error_border);
+            check =  false;
+        }else {
+            password.setError(null);
+            password.clearFocus();
+            password.setBackgroundResource(R.drawable.success_border);
+        }
+
+        if (TextUtils.isEmpty(serial_id.getText())) {
+            serial_id.setError("Serial ID should not be Empty");
+            serial_id.setBackgroundResource(R.drawable.error_border);
+            check =  false;
+        }else {
+            serial_id.setError(null);
+            serial_id.clearFocus();
+            serial_id.setBackgroundResource(R.drawable.success_border);
+        }
+        return check;
+    }
+
+}
