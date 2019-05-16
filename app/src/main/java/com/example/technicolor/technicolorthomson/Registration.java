2eac1a999b2fed2eac3da46baeadab5fc9e16edc
Nagarjun <arjunraina64@gmail.com>
2019-05-16 15:58:46 +0530

    removed main file

--- app/src/main/java/com/example/technicolor/technicolorthomson/Registration.java
+++ app/src/main/java/com/example/technicolor/technicolorthomson/Registration.java
@@ -65,7 +65,6 @@ public class Registration extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         // super.onBackPressed(); commented this line in order to disable back press
-        //Write your code here
         Toast.makeText(getApplicationContext(), "Back press disabled!", Toast.LENGTH_SHORT).show();
     }
 
