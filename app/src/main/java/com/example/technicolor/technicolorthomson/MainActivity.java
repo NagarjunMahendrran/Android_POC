d5eae709642ef0e4106075484fd3c235bbade91e
Nagarjun <arjunraina64@gmail.com>
2019-05-16 21:08:45 +0530

    removed main file

--- app/src/main/java/com/example/technicolor/technicolorthomson/MainActivity.java
+++ app/src/main/java/com/example/technicolor/technicolorthomson/MainActivity.java
@@ -64,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         // super.onBackPressed(); commented this line in order to disable back press
-        //Write your code here
         Toast.makeText(getApplicationContext(), "Back press disabled!", Toast.LENGTH_SHORT).show();
     }
 
